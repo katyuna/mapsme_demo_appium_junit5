a3d4a0a6fee51147acbb8bbc671d947fcd333fd5
Ekaterina Zarubina <edubovenko@gmail.com>
2022-06-20 19:26:07 +0700

    open app

--- src/test/java/mainscreen/MainScreenTest.java
+++ src/test/java/mainscreen/MainScreenTest.java
@@ -1,7 +1,9 @@
 package mainscreen;
 
+import io.appium.java_client.android.AndroidDriver;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -18,7 +20,7 @@ public class MainScreenTest {
     public void testMainPage() {
 
         // Устанавливаем Android Driver:
-        AndroidDriver<AndroidElement> driver = null;
+        AndroidDriver driver = null;
 
         //Задаем параметры
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -27,13 +29,13 @@ public class MainScreenTest {
         capabilities.setCapability("udid", "f0c565e9");
         //Cсылка на APK, который нужно будет запускать
         // .apk должен быть debug сборкой, чтобы вы и appium могли делать inspect приложения
-        capabilities.setCapability("app", "D:\\SoftWareCats\\Projects\\Maps.me\\apk\\v13.0.71324-googleRelease.zip\\android\\app\\build\\outputs\\apk\\google\\releasemapsme-android-googleRelease-vc71324-17062022.apk");
-        //capabilities.setCapability("app", "com.mapswithme.maps.pro.kode");
+        capabilities.setCapability("appPackage", "com.mapswithme.maps.pro");
+        capabilities.setCapability("appActivity", "com.mapswithme.maps.MainActivity");
 
 
 //Запускаем Appium драйвер
         try {
-            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+            driver = new AndroidDriver (new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         } catch (MalformedURLException e) {
             System.out.println(e.getMessage());
@@ -42,7 +44,7 @@ public class MainScreenTest {
         //        AndroidDriver driver = new AndroidDriver(remoteUrl, capabilities);
         TouchActions touchActions = new TouchActions(driver);
 
-        touchActions.singleTap(driver.findElement(By.id("00000000-0000-00ab-0000-000400000004")));
+        touchActions.singleTap(driver.findElement(By.id("00000000-0000-01cf-3b9a-ca2b00000004")));
 
     }
 }
