ef869642170a589f55d3eff5b84f391f6f549a43
Ekaterina Zarubina <edubovenko@gmail.com>
2022-07-28 12:22:23 +0700

    conflicts

--- src/test/java/com/mainscreen/RouteTest.java
+++ src/test/java/com/mainscreen/RouteTest.java
@@ -1,9 +1,10 @@
-package com.mainscreen;
+/*package com.mainscreen;
 
 import com.BaseTest;
 import com.po.MainScreen;
 import com.po.RouteBottomSheet;
 import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 
@@ -12,21 +13,29 @@ import static org.junit.Assert.assertTrue;
 public class RouteTest extends BaseTest {
     MainScreen mainScreen = new MainScreen(driver);
     RouteBottomSheet routeBottomSheet = new RouteBottomSheet(driver);
-
     @Test
+    @DisplayName("seed")
+    @Description ("eee")
+    public void testRoute() {
+        testWaitRouteButton();
+        testRouteOpen();
+    }
+
+
+    @Step
     @DisplayName("Wait route button")
     @Description("Wait until rout button displays")
     public void testWaitRouteButton() {
         mainScreen.isRouteButton();
     }
 
-    @Test
+    @Step
     @DisplayName("Open route bottom sheet")
     @Description("Click route button and open route bottom sheet")
-    public void testRoute() {
+    public void testRouteOpen() {
         mainScreen.clickRouteButton();
         boolean routeSheetIsDisplayed = routeBottomSheet.isRouteTitle();
         assertTrue("Error: route bottom sheet doesn't displays", routeSheetIsDisplayed);
 
     }
-}
+}*/
