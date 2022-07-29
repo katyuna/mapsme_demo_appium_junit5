7a51a610dc421d70f68f795c18f6af9d3893ba39
Ekaterina Zarubina <edubovenko@gmail.com>
2022-07-29 12:50:59 +0700

    update appium lib to 8.1.1 and replace AndroidElement by WebElement

--- src/main/java/po/RouteBottomSheet.java
+++ src/main/java/po/RouteBottomSheet.java
@@ -1,9 +1,9 @@
 package po;
 
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 
@@ -20,7 +20,7 @@ public class RouteBottomSheet {
 
     //Search categories sheet header
     @AndroidFindBy(xpath = ".//android.widget.TextView[@text='Route']")
-    private AndroidElement routeTitle;
+    private WebElement routeTitle;
 
     //Is route title displayed
     public boolean isRouteTitle() {
