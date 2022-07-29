35d5c4429ca6007abb8497b29e92b19dfce28cb3
Ekaterina Zarubina <edubovenko@gmail.com>
2022-07-29 11:31:18 +0700

    problem with library versions

--- src/main/java/com/po/MainScreen.java
+++ src/main/java/com/po/MainScreen.java
@@ -4,8 +4,6 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.support.PageFactory;
-
 
 public class MainScreen {
     private AndroidDriver driver;
